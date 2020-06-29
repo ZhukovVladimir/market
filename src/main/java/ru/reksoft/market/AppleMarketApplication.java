277860b5527ac95064e7c317c7c196f582802538
unknown <fwsurprise@gmail.com>
2020-06-29 18:43:35 +0300

    added swagger-doc

--- src/main/java/ru/reksoft/market/AppleMarketApplication.java
+++ src/main/java/ru/reksoft/market/AppleMarketApplication.java
@@ -10,8 +10,10 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -37,6 +39,14 @@ public class AppleMarketApplication {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("ru.reksoft"))
 				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder()
+				.title("Apple Market REST API")
+				.description("\"Spring Boot REST API for Online Store\"")
 				.build();
 	}
 
--- src/main/java/ru/reksoft/market/controller/api/admin/ProductAdminController.java
+++ src/main/java/ru/reksoft/market/controller/api/admin/ProductAdminController.java
@@ -1,12 +1,18 @@
 package ru.reksoft.market.controller.api.admin;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import ru.reksoft.market.data.dto.ProductDto;
 import ru.reksoft.market.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
+@Api(value = "/api/admin/products", tags = {"Контроллер для редактирования продуктов"})
 @RestController
 @RequestMapping(value = "/api/admin/products")
 public class ProductAdminController {
@@ -18,17 +24,20 @@ public class ProductAdminController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "Создание продукта")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProductDto createProduct(@Validated @RequestBody ProductDto product) {
         return productService.createProduct(product);
     }
 
+    @ApiOperation(value = "Редактирование продукта")
     @PutMapping("/{id}")
     public ProductDto updateProduct(@PathVariable Long id, @Validated @RequestBody ProductDto product) {
         return productService.updateProduct(id, product);
     }
 
+    @ApiOperation(value = "Удаление продукта")
     @DeleteMapping("/{id}")
     public ProductDto deleteProduct(@PathVariable Long id) {
         return productService.deleteProduct(id);
